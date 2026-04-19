d7b2041e23d2af2d96c3a109445baa5e8ca677b2
ktc25a31l0001-0115 <ktc25a31l0001@edu.kyoto-tech.ac.jp>
2026-04-19 10:51:21 +0900

    Initial L-chika success

---
+++ platform/esp32_tx/main.cpp
@@ -0,0 +1,19 @@
+#include "Arduino.h"
+
+#define LED_PIN 2
+
+void setup(){
+  /*Serial.begin(115200);
+  delay(100);
+  Serial.printf("%s - run\n",__func__);
+  pinMode(LED_PIN, OUTPUT);*/
+}
+
+void loop(){
+  /*digitalWrite(LED_PIN, HIGH);
+  Serial.printf("%s - LED_PIN - HIGH\n",__func__);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  Serial.printf("%s - LED_PIN - LOW\n",__func__);
+  delay(3000);*/
+}
