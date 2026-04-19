31caa8030f385ea92051e66078dda20adf119691
ktc25a31l0001-0115 <ktc25a31l0001@edu.kyoto-tech.ac.jp>
2026-04-19 10:54:09 +0900

    initial L-chika success

--- platform/esp32_tx/main.cpp
+++ platform/esp32_tx/main.cpp
@@ -3,17 +3,17 @@
 #define LED_PIN 2
 
 void setup(){
-  /*Serial.begin(115200);
+  Serial.begin(115200);
   delay(100);
   Serial.printf("%s - run\n",__func__);
-  pinMode(LED_PIN, OUTPUT);*/
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop(){
-  /*digitalWrite(LED_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
   Serial.printf("%s - LED_PIN - HIGH\n",__func__);
   delay(1000);
   digitalWrite(LED_PIN, LOW);
   Serial.printf("%s - LED_PIN - LOW\n",__func__);
-  delay(3000);*/
+  delay(3000);
 }
