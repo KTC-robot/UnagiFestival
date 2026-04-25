7af5a5899229064fe02bbb32b98d11a5ff77c769
ktc25a31l0001-0115 <ktc25a31l0001@edu.kyoto-tech.ac.jp>
2026-04-25 18:42:12 +0900

    serial reception

--- platform/esp32_tx/main.cpp
+++ platform/esp32_tx/main.cpp
@@ -4,16 +4,18 @@
 
 void setup(){
   Serial.begin(115200);
-  delay(100);
+  delay(1000);
   Serial.printf("%s - run\n",__func__);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop(){
-  digitalWrite(LED_PIN, HIGH);
-  Serial.printf("%s - LED_PIN - HIGH\n",__func__);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  Serial.printf("%s - LED_PIN - LOW\n",__func__);
-  delay(3000);
+  if (Serial.available() > 0) {
+    char reception = Serial.read();
+    Serial.printf("%c", reception);
+    digitalWrite(LED_PIN, HIGH);
+    delay(800);
+    digitalWrite(LED_PIN, LOW);
+    delay(800);
+  }
 }
