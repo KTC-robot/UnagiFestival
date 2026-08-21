6f28a743cf86daeacf77dede1f08ad19c5f54f54
blazex60 <ktc25a31k0001@edu.kyoto-tech.ac.jp>
2026-08-21 19:29:01 +0900

    fix:ゲイン調整

--- platform/chassis_ctrl/constants.hpp
+++ platform/chassis_ctrl/constants.hpp
@@ -64,11 +64,11 @@ constexpr float DEFAULT_WHEEL_GAIN_BWD[NUM_WHEELS] = {
 };
 
 constexpr float DEFAULT_WHEEL_GAIN_RIGHT[NUM_WHEELS] = {
-  1.200f, 1.200f, 1.000f, 1.000f
+  0.450f, 0.450f, 1.000f, 1.000f
 };
 
 constexpr float DEFAULT_WHEEL_GAIN_LEFT[NUM_WHEELS] = {
-  1.200f, 1.200f, 1.000f, 1.000f
+  0.450f, 0.450f, 1.000f, 1.000f
 };
 
 }
